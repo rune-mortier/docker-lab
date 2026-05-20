9a8f1992bce287a2f9d2653126c136eb90e4da1e
Rune Mortier <rune.mortier@student.howest.be>
2026-05-20 11:59:38 +0200

    Fix CRLF line endings and add db wait loop in prestart.sh

--- api/prestart.sh
+++ api/prestart.sh
@@ -1,5 +1,19 @@
 #! /usr/bin/env sh
 
+# Wait for database connection
+
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+
+sleep 10
+
+printf "Database %s:%s not ready" "$POSTGRES_HOST" "$POSTGRES_PORT"
+
+done
+
+# Make sure database is ready to accept connections
+
+sleep 10
+
 alembic upgrade head
 
 exec "$@"
